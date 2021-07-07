9859a47f8fdf99450d0b0899f518f9dc57620c53
behrndc <behrndc@wwu.edu>
2021-07-07 13:23:28 -0700

    added temp player movement

---
+++ Assets/Scripts/characterMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    public float jumpHeight;
+    public float stepDown;
+    public float gravity;
+    Vector2 input;
+    Animator animator;
+    CharacterController characterController;
+    Vector3 rootMotion;
+    Vector3 velocity;
+    bool isJumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("MoveX", input.x);
+        animator.SetFloat("MoveZ", input.y);
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            Jump();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (isJumping)
+        {
+            velocity.y -= gravity * Time.fixedDeltaTime;
+            characterController.Move(velocity * Time.fixedDeltaTime);
+            isJumping = !characterController.isGrounded;
+            animator.SetBool("isJumping", isJumping);
+
+        }
+        else
+        {
+            characterController.Move(rootMotion + Vector3.down * stepDown);
+            rootMotion = Vector3.zero;
+        }
+    }
+
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+            velocity = animator.velocity;
+            velocity.y = Mathf.Sqrt(2 * gravity * jumpHeight);
+        }
+    }
+}
---
+++ Assets/pointAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointAt : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the camera every frame so it keeps looking at the target
+        transform.LookAt(target);
+
+        // Same as above, but setting the worldUp parameter to Vector3.left in this example turns the camera on its side
+        transform.LookAt(target, Vector3.up);
+        // transform.rotation.z = 0;
+    }
+}
