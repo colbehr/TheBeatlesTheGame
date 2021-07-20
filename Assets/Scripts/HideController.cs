267daadf58a36a63d63e82c92ee4732c5f290ef4
behrndc <behrndc@wwu.edu>
2021-07-19 17:11:37 -0700

    added a working hiding mechanic when e is pressed on pot, still needs ui adjustment

---
+++ Assets/Scripts/HideController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideController : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Camera HideCamera;
+    public float rotSpeed = 100f;
+    public GameObject Player;
+
+    bool canEnter = false;
+    bool inHiding = false;
+    GameObject HideAnimate;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideAnimate = this.gameObject.transform.GetChild(0).gameObject;
+        anim = HideAnimate.GetComponent<Animator>();
+        MainCamera = Camera.main;
+        MainCamera.enabled = true;
+        HideCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera rotate according to mouse
+        float h = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
+        HideCamera.transform.Rotate(0, h, 0);
+        interact();
+
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.tag == "Player")
+        {
+            canEnter = true;
+            anim.SetBool("Open", true);
+            //show hide ui
+        }
+    }
+    void OnTriggerExit(Collider player)
+    {
+        if (player.tag == "Player")
+        {
+            canEnter = false;
+            anim.SetBool("Open", false);
+            // hide hide ui
+        }
+    }
+
+
+    void interact()
+    {
+        // change main camera to this camera 
+        if (Input.GetKeyDown(KeyCode.E) && !inHiding)
+        {
+            // hide player 
+            Player.SetActive(false);
+            print("Player Gets in");
+            MainCamera.enabled = false;
+            HideCamera.enabled = true;
+            inHiding = true;
+        }
+        else if (inHiding && Input.GetKeyDown(KeyCode.E))
+        {
+            Player.SetActive(true);
+            print("Player Leaves");
+            MainCamera.enabled = true;
+            HideCamera.enabled = false;
+            inHiding = false;
+        }
+    }
+}
--- Assets/Scripts/characterMovement.cs
+++ Assets/Scripts/characterMovement.cs
@@ -13,39 +13,56 @@ public class characterMovement : MonoBehaviour
     Vector3 rootMotion;
     Vector3 velocity;
     bool isJumping;
+    public float turn = 15;
+    Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+        //lock cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //get x y input from keyboard
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
-
+        //set params for animations
         animator.SetFloat("MoveX", input.x);
         animator.SetFloat("MoveZ", input.y);
-
+        //if the character is not moving, let user control where the camera sits
+        if (input.x  > .1 || input.y > .1 )
+        {
+            //otherwise the character turns to where the camera is pointing
+            float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turn * Time.deltaTime);
+        }
+        //if the player hits the jump key, jump
         if (Input.GetAxis("Jump") > 0)
         {
             Jump();
         }
     }
+
     void FixedUpdate()
     {
+        //if the character is jumping, bring the character back to the ground and play the jump anim
         if (isJumping)
         {
             velocity.y -= gravity * Time.fixedDeltaTime;
             characterController.Move(velocity * Time.fixedDeltaTime);
             isJumping = !characterController.isGrounded;
             animator.SetBool("isJumping", isJumping);
-
         }
         else
         {
+            //otherwise move the character
             characterController.Move(rootMotion + Vector3.down * stepDown);
             rootMotion = Vector3.zero;
         }
@@ -54,6 +71,7 @@ public class characterMovement : MonoBehaviour
 
     void Jump()
     {
+        //dont let the character jump multiple times
         if (!isJumping)
         {
             isJumping = true;
