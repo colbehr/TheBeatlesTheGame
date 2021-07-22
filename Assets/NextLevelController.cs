14a73d1ce552901445a4504139a0a38597162012
behrndc <behrndc@wwu.edu>
2021-07-22 12:13:36 -0700

    added alarm bot and one last script

---
+++ Assets/NextLevelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelController : MonoBehaviour
+{
+
+    public string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        print("Next level UI");
+        print("Press 'E' to go to the next level.");
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+
+}
