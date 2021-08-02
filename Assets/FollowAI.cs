057d537dc971a3f3da24a50a588c3d9802b59e80
behrndc <behrndc@wwu.edu>
2021-08-02 13:22:03 -0700

    added follow ai to 4 characters in test level

---
+++ Assets/FollowAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FollowAI : MonoBehaviour
+{
+    public GameObject followObject;
+    NavMeshAgent agent;
+    Animator animator;
+
+    public float maxTime = 1;
+    float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0.0f)
+        {
+            agent.destination = followObject.transform.position;    
+            timer = maxTime;        
+        }
+        animator.SetFloat("MoveX", agent.velocity.x);
+        animator.SetFloat("MoveZ", agent.velocity.z);
+        
+    }
+}
