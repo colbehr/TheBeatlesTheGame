f71db1062991014bd396e1628026b62338a415f8
behrndc <behrndc@wwu.edu>
2021-08-04 17:43:45 -0700

    tweaked detection to use main ui eyes

--- Assets/Scripts/DetectionController.cs
+++ Assets/Scripts/DetectionController.cs
@@ -7,6 +7,7 @@ public class DetectionController : MonoBehaviour
     private GameObject[] meanies;
     private AudioSource music;
     private GameObject eye;
+    private PlayerState playerState;
     public AudioClip suspense;
     public AudioClip safe;
 
@@ -15,7 +16,9 @@ public class DetectionController : MonoBehaviour
     {
         meanies = GameObject.FindGameObjectsWithTag("Meanie");
         music = GetComponent<AudioSource>();
-        eye = GameObject.Find("Canvas").transform.Find("Image").gameObject;
+        playerState = GameObject.FindWithTag("Player").gameObject.GetComponent<PlayerState>();
+        // eye = GameObject.Find("Canvas").transform.Find("Image").gameObject;
+
     }
 
     // Update is called once per frame
@@ -27,13 +30,15 @@ public class DetectionController : MonoBehaviour
             if(meanie.GetComponent<Meanie>().chasing == true)
             {
                 chase = true;
+                
                 break;
             }
         }
 
         if (chase)
         {
-            eye.SetActive(true);
+            // eye.SetActive(true);
+            playerState.hidden = false;
             if (music.clip.name == "Pepperland" || music.clip.name == "Sea Of Time")
             {
                 music.clip = suspense;
@@ -43,7 +48,8 @@ public class DetectionController : MonoBehaviour
         }
         else
         {
-            eye.SetActive(false);
+            // eye.SetActive(false);
+            playerState.hidden = true;
             if (music.clip.name == "March of the Meanies")
             {
                 music.clip = safe;
